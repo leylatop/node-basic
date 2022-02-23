6acbae3998e32f11a85bb5128e485bc8729f607d
qiaoxx <775177772@qq.com>
2022-02-23 10:12:33 +0800

    add: npm promises-aplus-tests

--- my-promise/2.promise-chaining.js
+++ my-promise/2.promise-chaining.js
@@ -15,14 +15,19 @@ function resolvePromise(promise, x, resolve, reject) {
   // x 是每次 .then的 onFulFilled / onRejected的调用结果
   // promise 是 每次 .then 调用完，return的新promise
   if ((typeof x === 'object' && x !== null) || typeof x === 'function') {
+    let called = false  // 防止重复调用resolve和reject
     try {
       const then = x.then
       if (typeof then === 'function') { // x中的then可能不是 promise中的then方法，而是通过Object.defineProperty 定义了一个then属性
         // 此时认为 x 是一个promise，并执行x的 then 方法
         // x.then(y => {},r=> {})
         then.call(x, y => {
+          if (called) return
+          called = true
           resolvePromise(promise, y, resolve, reject) // 递归解析y的值
         }, r => { // 失败时，不需要递归解析，直接向上抛出错误
+          if (called) return
+          called = true
           reject(r)
         })
       } else {
@@ -30,6 +35,8 @@ function resolvePromise(promise, x, resolve, reject) {
         resolve(x)
       }
     } catch (e) {
+      if (called) return
+      called = true
       reject(e)
     }
   } else {
@@ -70,6 +77,10 @@ class Promise {
   }
 
   then(onFulFilled, onRejected) {   // 调用then时，返回了new Promise，以此实现promise的链式调用
+    // 对 onFulFilled 和 onRejected 不为函数时的默认赋值
+    onFulFilled = typeof onFulFilled === 'function' ? onFulFilled : v => v
+    onRejected = typeof onRejected === 'function' ? onRejected : e => { throw e }
+
     // 调用then的时候会立即执行resolve/reject
     // new Promise 实例化的时候，也会立即执行new Promise 参数
     // 所以就把 里层new Promise 参数 执行器 作为外层的then的执行域
@@ -135,7 +146,25 @@ class Promise {
   }
 }
 
+// 此方法可以用来进行手写promise的规范测试，使用npm包为 promises-aplus-tests
+Promise.deferred = function() {
+  let dfd = {}
+  dfd.promise = new Promise((resolve, reject) => {
+    dfd.resolve = resolve
+    dfd.reject = reject
+  })
+  return  dfd
+}
 
+// 必须暴露一下才能进行 promises-aplus-tests 测试
+module.exports = Promise
+
+
+
+
+/**
+ * 测试场景
+ */
 const p1 = new Promise((resolve, reject) => {
   setTimeout(() => {
     resolve(100)
